8025d46561a322a1a58c950c29d00249485befb1
farhanrashad <farhan@dynexcel.com>
2019-10-28 07:38:17 +0000

    update

--- de_product_dimensions/models/.ipynb_checkpoints/models-checkpoint.py
+++ de_product_dimensions/models/.ipynb_checkpoints/models-checkpoint.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.addons import decimal_precision as dp
 
 class Product(models.Model):
     _inherit = 'product.template'
@@ -9,4 +10,4 @@ class Product(models.Model):
     width = fields.Integer(string='Width')
     length = fields.Integer(string='Length')
     height = fields.Integer(string='Height')
-    dim_weight = fields.Float(string='Weight/Kg')
+    dim_weight = fields.Float(string='Weight/Kg',digits=dp.get_precision('Stock Weight'))
