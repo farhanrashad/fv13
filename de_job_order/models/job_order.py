d12338a8716e6b260e9f4e35e6f5287bb2c9e105
farhanrashad <farhan@dynexcel.com>
2019-10-31 14:03:01 +0000

    update

--- de_job_order/models/job_order.py
+++ de_job_order/models/job_order.py
@@ -2,7 +2,8 @@
 
 from datetime import datetime
 from odoo import api, fields, models, _
-from odoo.exceptions import UserError, ValidationError
+from odoo.exceptions import UserError, ValidationError, Warning
+
 
 from odoo.addons import decimal_precision as dp
 
@@ -19,7 +20,13 @@ class JobOrder(models.Model):
         help='Defines the rules that have to be applied to this Job Order, accordingly ')
     
     sale_id = fields.Many2one('sale.order', 'Order Reference',required=False)
-    state = fields.Selection([('draft', 'Draft'),('confirmed', 'Confirmed'),('processed', 'Processed'),('done', 'Done'),        ('cancel', 'Cancelled')], string='Status', readonly=True, copy=False, index=True, tracking=3, default='draft')
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('confirmed', 'Confirmed'),
+        ('planned', 'Planned'),
+        ('processed', 'Processed'),
+        ('done', 'Done'),
+        ('cancel', 'Cancelled')], string='Status', readonly=True, copy=False, index=True, tracking=3, default='draft')
     
     date_order = fields.Datetime(string='Order Date', required=True, readonly=True, index=True, states={'draft': [('readonly', False)], 'sent': [('readonly', False)]}, copy=False, default=fields.Datetime.now)
     
@@ -33,12 +40,106 @@ class JobOrder(models.Model):
     
     details_by_component_category = fields.One2many('job.order.line', compute='_compute_details_by_component_category', string='Details by Salary Rule Category')
     
+    bom_ids = fields.One2many('mrp.bom', 'product_tmpl_id', string='Bill of Materials')
+    bom_count = fields.Integer(string='BOMs', compute='_compute_bom_ids')
+    
+    @api.depends('bom_ids')
+    def _compute_bom_ids(self):
+        b = 0
+        for order in self:
+            for line in order.job_order_lines:
+                b = len(line.product_tmpl_id.bom_ids)
+            order.bom_count = b
+    
+    def action_view_bom(self):
+        action = self.env.ref('mrp.mrp_bom_tree_view').read()[0]
+
+        boms = self.mapped('boms_ids')
+        if len(boms) > 1:
+            action['domain'] = [('id', 'in', boms.ids)]
+        elif boms:
+            action['views'] = [(self.env.ref('mrp.mrp_bom_form_view').id, 'form')]
+            action['res_id'] = boms.id
+        return action
+    
+    
     @api.model
     def create(self, vals):
         vals['name'] = self.env['ir.sequence'].get('job.order') or ' '
         res_id = super(JobOrder, self).create(vals)
         return res_id
     
+    def generate_orders(self):
+        bom_ids = []
+        primary_bom_ids = []
+        all_boms = []
+        product_id = 0
+        var1 = ''
+        #self.state = 'processed'
+        #finish_production_id = self.env['mrp.production']
+        #semi_production_id = self.env['mrp.production']
+        for line in self.job_order_sale_lines:
+            #bom_ids = self.env['mrp.bom'].browse(line.bom_id).id
+            fvals = {
+                'product_id': line.product_id.id,
+                'product_uom_id':line.product_id.uom_id.id,
+                'product_qty':line.product_uom_qty,
+                'bom_id':line.bom_id.id,
+                'origin':self.sale_id.name,
+                #'orderpoint_id':self.sale_id.id,
+                'job_order_id': self.id,
+                'ref_sale_id':self.sale_id.id,
+            }
+            finish_production_id = self.env['mrp.production'].create(fvals)
+            self.env.cr.commit()
+
+            if not (line.bom_id in bom_ids):
+                bom_ids.append(line.bom_id)
+            
+        for boms in bom_ids:
+            #var1 = var1 + ',' + p
+            #var1 = var1 + '--' + str(boms.id)
+            all_boms += boms._recursive_boms()
+            #var1 = var1 + ',' + str(all_boms)
+            primary_bom_ids.append(boms.id)
+            var1 = str(primary_bom_ids)
+        sub_boms = self.env['mrp.bom'].search([('id', 'in', all_boms ),('id', 'not in', primary_bom_ids )])
+        for bom in sub_boms:
+            var1 = var1 + str(bom.id)
+            product_id = self.env['product.product'].search([('product_tmpl_id', '=', bom.product_tmpl_id.id )])
+            if bom.type == 'normal':
+                svals = {
+                    'product_id': product_id.id,
+                    'product_uom_id':product_id.uom_id.id,
+                    'product_qty':1,
+                    'bom_id':bom.id,
+                    'origin':self.sale_id.name,
+                    #'orderpoint_id':self.sale_id.id,
+                    'procurement_group_id':self.sale_id.id,
+                    'job_order_id': self.id,
+                    'ref_sale_id':self.sale_id.id,
+                }
+                semi_production_id = self.env['mrp.production'].create(svals)
+                self.env.cr.commit()
+
+            elif bom.type == 'subcontract':
+                vals = {
+                    'partner_id': [0,bom.subcontractor_ids.id],
+                    'group_id': self.sale_id.id,
+                }
+                purchase_id = self.env['purchase.order'].create(vals)
+                self.env.cr.commit()
+                line_val = {
+                    'order_id':purchase_id.id,
+                    'product_id':product_id.id,
+                    'product_uom':product_id.uom_po_id.id,
+                    'product_qty':1,
+                }
+                purchase_line_id = self.env['purchase.order.line'].create(line_val)
+                self.env.cr.commit()
+       
+        raise Warning(_(var1))
+        
     def compute_job(self):
         self.job_order_lines.unlink()
         job_order_line = self.env['job.order.line']
@@ -50,7 +151,6 @@ class JobOrder(models.Model):
                         'job_order_id':job.id,
                         'job_sale_line_id':sale.id,
                         'name':rule.name,
-                        'code':rule.code,
                         'sequence':rule.sequence,
                         'category_id':rule.category_id.id,
                         'job_rule_id':rule.id,
@@ -58,7 +158,7 @@ class JobOrder(models.Model):
                         'quantity':sale.product_uom_qty,
                     }
                     job_order_line.create(result_dict)
-        self.state = 'processed'
+        #self.state = 'planned'
     
     def generate_sale_lines(self):
         vals = {}
@@ -74,10 +174,13 @@ class JobOrder(models.Model):
                     'product_uom': line.product_uom.id,
                     'sale_line_id': line.id,
                     'struct_id': self.struct_id.id,
+                    #'bom_id': self._get_bom_id(line.product_id.product_tmpl_id),
+                    'bom_id': line.product_id.product_tmpl_id.bom_ids.id,
                 }
                 job_sale_line.create(vals)
+                
         self.state = 'confirmed'
-            
+    
     def _compute_details_by_component_category(self):
         for job in self:
             job.details_by_component_category = job.mapped('job_order_lines').filtered(lambda line: line.category_id)
@@ -94,11 +197,6 @@ class JobOrderSaleLine(models.Model):
     job_order_id = fields.Many2one('Job.order', string='Job Order Reference', required=True, ondelete='cascade', index=True, copy=False, readonly=True)
     
     sale_line_id = fields.Many2one('sale.order.line', string='Order Line', required=True, index=True)
-    
-    code = fields.Char(required=False,
-        help="The code of Job rules can be used as reference in computation of other rules. "
-             "In that case, it is case sensitive.")
-    
     product_tmpl_id = fields.Many2one(
         'product.template', 'Product',
         domain="[('type', 'in', ['product', 'consu'])]", required=False)
@@ -110,6 +208,7 @@ class JobOrderSaleLine(models.Model):
     product_uom = fields.Many2one('uom.uom', string='Unit of Measure',change_default=True, default=_get_default_uom)
     product_uom_qty = fields.Float(string='Ordered Quantity', digits=dp.get_precision('Product Unit of Measure'), required=True, default=1.0)
     struct_id = fields.Many2one('job.order.structure', string='Structure', readonly=True)
+    bom_id = fields.Many2one('mrp.bom', string='BOM')
     
     @api.depends('product_uom_qty')
     def _get_secondary_qty(self):
@@ -133,3 +232,32 @@ class JobOrderLine(models.Model):
     job_rule_id = fields.Many2one('job.order.rule', 'Job Rule',required=True)
     line_desc = fields.Char(string='Description')
     quantity = fields.Float(string='Quantity', required=True, digits=dp.get_precision('Product Unit of Measure'), default=1.0)
+    
+
+class MRPBomLine(models.Model):
+    _inherit = 'mrp.bom.line'
+    
+    
+    
+class MRPBom(models.Model):
+    _inherit = 'mrp.bom'
+    
+    def _recursive_boms(self):
+        """
+        @return: returns a list of tuple (id) which are all the children of the passed bom_ids
+        """
+        children_boms = []
+        for bom in self.filtered(lambda bom: bom.bom_line_ids.product_id.product_tmpl_id.bom_ids):
+            children_boms += bom.bom_line_ids.product_id.product_tmpl_id.bom_ids._recursive_boms()
+        return [(bom.id) for bom in self] + children_boms
+    
+class MRPProduction(models.Model):
+    _inherit = 'mrp.production'
+    
+    job_order_id = fields.Many2one('Job.order', string='Job Order Reference', required=False, ondelete='cascade', index=True, copy=False, readonly=True)
+    ref_sale_id = fields.Many2one('sale.order', string='Sale Order', required=False, ondelete='cascade', index=True, copy=False, readonly=True)
+    
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+    
+    job_order_id = fields.Many2one('Job.order', string='Job Order Reference', required=False, ondelete='cascade', index=True, copy=False, readonly=True)
