3f53b0209ea55d08f6ba0cb42ac9a445e3361933
farhanrashad <farhan@dynexcel.com>
2019-11-03 11:02:46 +0000

    add product weight

--- de_product_weight/models/picking.py
+++ de_product_weight/models/picking.py
@@ -8,7 +8,7 @@ from odoo.exceptions import UserError, ValidationError, Warning
 from odoo.addons import decimal_precision as dp
 
 class StockMove(models.Model):
-    _inherit = 'stock.move'
+    _inherit = 'stock.move.line'
     
     weight = fields.Float(related='product_id.weight',string='Weight/kg',readonly=True, store=True)
     total_weight = fields.Float('Total Weight', digits=dp.get_precision('Stock Weight'), help="Weight of the product in order line")
