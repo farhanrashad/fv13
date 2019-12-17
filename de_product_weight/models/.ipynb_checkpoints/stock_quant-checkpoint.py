93ddc6750fde84a8f7893543e159356334c2a88f
farhanrashad <farhan@dynexcel.com>
2019-12-17 00:07:20 +0000

    update product weight

--- de_product_weight/models/.ipynb_checkpoints/stock_quant-checkpoint.py
+++ de_product_weight/models/.ipynb_checkpoints/stock_quant-checkpoint.py
@@ -11,15 +11,4 @@ class StockQuant(models.Model):
     _inherit = 'stock.quant'
     
     product_weight = fields.Float('Total Weight')    
-    po_uom_stk = fields.Float(string='PO UOM Stock', readonly=False, compute='_calculate_po_uom_stock', default=0.0)
-    uom_po_id = fields.Many2one('uom.uom', 'PUOM',related='product_id.product_tmpl_id.uom_po_id', readonly=True)
     
-    def _calculate_po_uom_stock(self):
-        for rs in self:
-            if rs.product_id.product_tmpl_id.uom_po_id.uom_type == 'bigger':
-                rs.po_uom_stk = rs.inventory_quantity / rs.product_id.product_tmpl_id.uom_po_id.factor_inv
-            elif rs.product_id.product_tmpl_id.uom_po_id.uom_type == 'smaller':
-                rs.po_uom_stk = rs.inventory_quantity * rs.product_id.product_tmpl_id.uom_po_id.factor_inv
-            else:
-                rs.po_uom_stk = rs.inventory_quantity
-                #rs.po_uom_stk = 2
