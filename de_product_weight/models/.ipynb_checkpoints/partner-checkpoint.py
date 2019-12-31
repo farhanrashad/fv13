333bf5603952ac072f255804adc9101ec61be607
farhanrashad <farhan@dynexcel.com>
2019-12-31 00:25:42 +0000

    change label

--- de_product_weight/models/.ipynb_checkpoints/partner-checkpoint.py
+++ de_product_weight/models/.ipynb_checkpoints/partner-checkpoint.py
@@ -9,5 +9,5 @@ from odoo.addons import decimal_precision as dp
 class Partner(models.Model):
     _inherit = 'res.partner'
     
-    is_purchase_weight = fields.Boolean("Compute prices in weight", default=False)
-    is_sale_weight = fields.Boolean("Compute prices in weight", default=False)
+    is_purchase_weight = fields.Boolean("Compute Prices in Weight", default=False)
+    is_sale_weight = fields.Boolean("Compute Prices in Weight", default=False)
