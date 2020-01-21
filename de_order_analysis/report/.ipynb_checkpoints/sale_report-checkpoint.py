bd6da60a5f650662c5104d82b4ba199fb4b5c395
farhanrashad <farhan@dynexcel.com>
2020-01-21 21:28:27 +0000

    order analysis

--- de_order_analysis/report/.ipynb_checkpoints/sale_report-checkpoint.py
+++ de_order_analysis/report/.ipynb_checkpoints/sale_report-checkpoint.py
@@ -12,7 +12,7 @@ class OrderAnalysis(models.Model):
     _rec_name = 'date'
     _order = 'date desc'
     
-    name = fields.Char('Order Reference', readonly=True)
+    name = fields.Char('Reference', readonly=True)
     date = fields.Datetime('Date', readonly=True)
     categ_id = fields.Many2one('product.category', 'Product Category', readonly=True)
     product_tmpl_id = fields.Many2one('product.template', 'Product', readonly=True)
@@ -25,6 +25,8 @@ class OrderAnalysis(models.Model):
     global_ref = fields.Char('Global Ref.', readonly=True)
     in_qty = fields.Float('In Qty', readonly=True)
     out_qty = fields.Float('Out Qty', readonly=True)
+    in_weight = fields.Float('In Weight', readonly=True)
+    out_weight = fields.Float('Out Weight', readonly=True)
     
     team_id = fields.Many2one('crm.team', 'Sales Team', readonly=True)
     partner_id = fields.Many2one('res.partner', 'Partner', readonly=True)
@@ -35,8 +37,8 @@ class OrderAnalysis(models.Model):
         with_ = ("WITH %s" % with_clause) if with_clause else ""
 
         select_ = """
-         count(x.*) as nbr, min(x.id) as id, x.global_ref, x.name as name,x.date, x.categ_id, x.product_tmpl_id, x.product_id, x.product_uom, sum(x.product_uom_qty) as product_uom_qty, sum(x.qty_delivered) as qty_delivered, sum(x.order_weight) as order_weight, x.job_order, sum(x.in_qty) as in_qty, sum(x.out_qty) as out_qty, x.team_id, x.partner_id, x.company_id, x.user_id from (
-            select l.id, o.global_ref, o.name as name,o.date_order as date, t.categ_id, m.product_tmpl_id, l.product_id as product_id, t.uom_id as product_uom, l.product_uom_qty, l.qty_delivered, l.total_weight as order_weight, j.name as job_order, 0 as in_qty, 0 as out_qty, o.team_id, o.partner_id, o.company_id, o.user_id
+         count(x.*) as nbr, min(x.id) as id, x.global_ref, x.name as name,x.date, x.categ_id, x.product_tmpl_id, x.product_id, x.product_uom, sum(x.product_uom_qty) as product_uom_qty, sum(x.qty_delivered) as qty_delivered, sum(x.order_weight) as order_weight, x.job_order, sum(x.in_qty) as in_qty, sum(x.out_qty) as out_qty, sum(x.in_weight) as in_weight, sum(x.out_weight) as out_weight, x.team_id, x.partner_id, x.company_id, x.user_id from (
+            select l.id, o.global_ref, o.name as name,o.date_order as date, t.categ_id, m.product_tmpl_id, l.product_id as product_id, t.uom_id as product_uom, l.product_uom_qty, l.qty_delivered, l.total_weight as order_weight, j.name as job_order, 0 as in_qty, 0 as out_qty, 0 as in_weight, 0 as out_weight, o.team_id, o.partner_id, o.company_id, o.user_id
 from job_order j
 join sale_order o on j.sale_id = o.id
 join res_partner p on o.partner_id = p.id
@@ -48,7 +50,7 @@ left join res_company b on o.company_id = b.id
 left join res_users u on o.user_id = u.id
 left join crm_team r on o.team_id = r.id
 union all
-select l.id, o.global_ref, o.name as name,o.date_order as date, t.categ_id, m.product_tmpl_id, l.product_id as product_id, t.uom_id as product_uom, l.product_uom_qty, l.qty_received as qty_delivered, l.total_weight as order_weight, j.name as job_order, 0 as in_qty, 0 as out_qty, p.team_id, o.partner_id, o.company_id, o.user_id
+select l.id, o.global_ref, o.name as name,o.date_order as date, t.categ_id, m.product_tmpl_id, l.product_id as product_id, t.uom_id as product_uom, l.product_uom_qty, l.qty_received as qty_delivered, l.total_weight as order_weight, j.name as job_order, 0 as in_qty, 0 as out_qty, 0 as in_weight, 0 as out_weight, p.team_id, o.partner_id, o.company_id, o.user_id
 from job_order j
 join purchase_order o on j.sale_id = o.id
 join res_partner p on o.partner_id = p.id
@@ -60,7 +62,7 @@ left join res_company b on o.company_id = b.id
 left join res_users u on o.user_id = u.id
 left join crm_team r on p.team_id = r.id
 union all
-select o.id, o.global_ref, o.name as name,o.date_start as date, t.categ_id, m.product_tmpl_id, o.product_id as product_id, t.uom_id as product_uom, o.product_uom_qty, (o.product_uom_qty - o.product_qty) as qty_delivered, o.production_weight as order_weight, j.name as job_order, 0 as in_qty, 0 as out_qty, p.team_id, s.partner_id, o.company_id, o.user_id
+select o.id, o.global_ref, o.name as name,o.date_start as date, t.categ_id, m.product_tmpl_id, o.product_id as product_id, t.uom_id as product_uom, o.product_uom_qty, (o.product_uom_qty - o.product_qty) as qty_delivered, o.production_weight as order_weight, j.name as job_order, 0 as in_qty, 0 as out_qty, 0 as in_weight, 0 as out_weight, p.team_id, s.partner_id, o.company_id, o.user_id
 from job_order j
 join sale_order s on j.sale_id = s.id
 join mrp_production o on o.job_order_id = j.id
@@ -73,8 +75,11 @@ left join res_users u on o.user_id = u.id
 left join crm_team r on p.team_id = r.id
 union all
 select l.id, so.global_ref, o.name as name, o.date_done, t.categ_id, m.product_tmpl_id, l.product_id as product_id, t.uom_id as product_uom, 0 as product_uom_qty, 0 as qty_delivered, 0 as order_weight, j.name as job_order, 
-(case when ld.usage='internal' then ml.product_uom_qty else 0 end) as in_qty, 
-(case when ls.usage='internal' then ml.product_uom_qty else 0 end) as out_qty, p.team_id, o.partner_id, o.company_id, o.user_id
+(case when ld.usage='internal' then ml.qty_done else 0 end) as in_qty, 
+(case when ls.usage='internal' then ml.qty_done else 0 end) as out_qty,
+(case when ld.usage='internal' then ml.total_weight else 0 end) as in_weight, 
+(case when ls.usage='internal' then ml.total_weight else 0 end) as out_weight,
+p.team_id, o.partner_id, o.company_id, o.user_id
 from stock_picking o
 join stock_move l on l.picking_id = o.id
 join stock_move_line ml on ml.move_id = l.id
@@ -91,7 +96,7 @@ join product_category c on t.categ_id = c.id
 left join res_company b on o.company_id = b.id
 left join res_users u on o.user_id = u.id
 left join crm_team r on p.team_id = r.id
-
+where l.state = 'done'
 ) x
 group by x.global_ref, x.name, x.date, x.categ_id, x.product_tmpl_id, x.product_id, x.product_uom,x.job_order,x.team_id,x.partner_id,x.company_id,x.user_id
 """
