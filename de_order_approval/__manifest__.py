378740391f5fcd98b3dab67ad9aeeee918592577
farhanrashad <farhan@dynexcel.com>
2020-03-05 20:32:12 +0000

    update order approval

--- de_order_approval/__manifest__.py
+++ de_order_approval/__manifest__.py
@@ -7,17 +7,20 @@
         """,
 
     'description': """
+    Sale Order Approval
+    =========================
         Sale Order Approval- Only users with particular group access will be able to approve the order.
     """,
 
     'author': "Dynexcel",
     'website': "http://www.dynexcel.com",
+    "live_test_url": "https://youtu.be/tNrBIrCzPPQ",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'sale',
-    'version': '0.4',
+    'version': '0.5',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'sale'],
@@ -26,14 +29,14 @@
     'data': [
         # 'security/ir.model.access.csv',
         'security/security.xml',
-        'views/views.xml',
-        'views/templates.xml',
+        'views/sale_views.xml',
         
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
+    "images":  ['static/description/banner.jpg'],
     'installable': True,
     'application': True,
     'auto_install': False,
