2c9fc31d742dc1a9a1e5d2a943abe94b6b3e2930
farhanrashad <farhan@dynexcel.com>
2020-04-14 18:35:15 +0000

    update weight

--- de_product_weight/models/.ipynb_checkpoints/stock_move-checkpoint.py
+++ de_product_weight/models/.ipynb_checkpoints/stock_move-checkpoint.py
@@ -12,8 +12,8 @@ class StockMove(models.Model):
             
     @api.depends('move_line_ids','move_line_nosuggest_ids','product_id')
     def _calculate_total_weight(self):
-        sum_weight = sum_weight1 = 0
         for line in self:
+            sum_weight = sum_weight1 = 0
             for move_line in line.move_line_ids:
                 if not move_line.total_weight:
                     sum_weight += move_line.qty_done * move_line.weight
