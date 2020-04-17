c4cae43ee5e0d5b11b44ab5a63dbb415cc69fad3
farhanrashad <farhan@dynexcel.com>
2020-04-17 12:15:30 +0000

    update product

--- de_product_weight/wizard/mrp_product_produce.py
+++ de_product_weight/wizard/mrp_product_produce.py
@@ -32,86 +32,31 @@ class MrpProductProduce(models.TransientModel):
                 total_weight += line.produced_weight
                     
                     
-    def _set_quantity_done(self, qty, weight):
-        """
-        Set the given quantity as quantity done on the move through the move lines. The method is
-        able to handle move lines with a different UoM than the move (but honestly, this would be
-        looking for trouble...).
-        @param qty: quantity in the UoM of move.product_uom
-        """
-        ml_weight = 0
-        for ml in self.move_line_ids:
-            ml_weight = ml.total_weight + weight
-            ml_qty = ml.product_uom_qty - ml.qty_done
-            if float_compare(ml_qty, 0, precision_rounding=ml.product_uom_id.rounding) <= 0:
-                continue
-            # Convert move line qty into move uom
-            if ml.product_uom_id != self.product_uom:
-                ml_qty = ml.product_uom_id._compute_quantity(ml_qty, self.product_uom, round=False)
-
-            taken_qty = min(qty, ml_qty)
-            # Convert taken qty into move line uom
-            if ml.product_uom_id != self.product_uom:
-                taken_qty = self.product_uom._compute_quantity(ml_qty, ml.product_uom_id, round=False)
-
-            # Assign qty_done and explicitly round to make sure there is no inconsistency between
-            # ml.qty_done and qty.
-            taken_qty = float_round(taken_qty, precision_rounding=ml.product_uom_id.rounding)
-            ml.qty_done += taken_qty
-            if ml.product_uom_id != self.product_uom:
-                taken_qty = ml.product_uom_id._compute_quantity(ml_qty, self.product_uom, round=False)
-            qty -= taken_qty
-
-            if float_compare(qty, 0.0, precision_rounding=self.product_uom.rounding) <= 0:
-                break
-        if float_compare(qty, 0.0, precision_rounding=self.product_uom.rounding) > 0:
-            vals = self._prepare_move_line_vals(quantity=0)
-            vals['qty_done'] = qty
-            vals['total_weight'] = ml_weight
-            ml = self.env['stock.move.line'].create(vals)
-    
     def _update_finished_move(self):
-        """ Update the finished move & move lines in order to set the finished
-        product lot on it as well as the produced quantity. This method get the
-        information either from the last workorder or from the Produce wizard."""
-        production_move = self.production_id.move_finished_ids.filtered(
-            lambda move: move.product_id == self.product_id and
-            move.state not in ('done', 'cancel')
-        )
-        if production_move and production_move.product_id.tracking != 'none':
-            if not self.finished_lot_id:
-                raise UserError(_('You need to provide a lot for the finished product.'))
-            move_line = production_move.move_line_ids.filtered(
-                lambda line: line.lot_id.id == self.finished_lot_id.id
-            )
-            if move_line:
-                if self.product_id.tracking == 'serial':
-                    raise UserError(_('You cannot produce the same serial number twice.'))
-                move_line.product_uom_qty += self.qty_producing
-                move_line.qty_done += self.qty_producing
-                move_line.total_weight += self.produced_weight
-            else:
-                location_dest_id = production_move.location_dest_id._get_putaway_strategy(self.product_id).id or production_move.location_dest_id.id
-                move_line.create({
-                    'move_id': production_move.id,
-                    'product_id': production_move.product_id.id,
-                    'lot_id': self.finished_lot_id.id,
-                    'product_uom_qty': self.qty_producing,
-                    'product_uom_id': self.product_uom_id.id,
-                    'qty_done': self.qty_producing,
-                    'total_weight': self.produced_weight,
-                    'location_id': production_move.location_id.id,
-                    'location_dest_id': location_dest_id,
-                })
-        else:
-            rounding = production_move.product_uom.rounding
-            production_move._set_quantity_done(
-                float_round(self.qty_producing, precision_rounding=rounding),self.produced_weight or 0.0
-            )
-            
-    
-                
-            
+        """ After producing, set the move line on the subcontract picking. """
+        res = super(MrpProductProduce, self)._update_finished_move()
+        if self.subcontract_move_id:
+            self.env['stock.move.line'].create({
+                'move_id': self.subcontract_move_id.id,
+                'picking_id': self.subcontract_move_id.picking_id.id,
+                'product_id': self.product_id.id,
+                'location_id': self.subcontract_move_id.location_id.id,
+                'location_dest_id': self.subcontract_move_id.location_dest_id.id,
+                'product_uom_qty': 0,
+                'total_weight': self.produced_weight,
+                'product_uom_id': self.product_uom_id.id,
+                'qty_done': self.qty_producing,
+                'lot_id': self.finished_lot_id and self.finished_lot_id.id,
+            })
+            if not self._get_todo(self.production_id):
+                ml_reserved = self.subcontract_move_id.move_line_ids.filtered(lambda ml:
+                    float_is_zero(ml.qty_done, precision_rounding=ml.product_uom_id.rounding) and
+                    not float_is_zero(ml.product_uom_qty, precision_rounding=ml.product_uom_id.rounding))
+                ml_reserved.unlink()
+                for ml in self.subcontract_move_id.move_line_ids:
+                    ml.product_uom_qty = ml.qty_done
+                self.subcontract_move_id._recompute_state()
+        return res
                         
 class MRPProductProduceLine(models.TransientModel):
     _inherit = 'mrp.product.produce.line'
