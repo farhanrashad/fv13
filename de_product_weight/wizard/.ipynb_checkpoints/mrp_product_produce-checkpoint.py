1e30356881d3fed80149a9dd6d002650ec572c3f
farhanrashad <farhan@dynexcel.com>
2020-04-18 21:36:40 +0000

    update weight

--- de_product_weight/wizard/.ipynb_checkpoints/mrp_product_produce-checkpoint.py
+++ de_product_weight/wizard/.ipynb_checkpoints/mrp_product_produce-checkpoint.py
@@ -33,30 +33,24 @@ class MrpProductProduce(models.TransientModel):
                     
                     
     def _update_finished_move(self):
-        """ After producing, set the move line on the subcontract picking. """
-        res = super(MrpProductProduce, self)._update_finished_move()
+        action = super(MrpProductProduce, self)._update_finished_move()
         if self.subcontract_move_id:
-            self.env['stock.move.line'].create({
-                'move_id': self.subcontract_move_id.id,
-                'picking_id': self.subcontract_move_id.picking_id.id,
-                'product_id': self.product_id.id,
-                'location_id': self.subcontract_move_id.location_id.id,
-                'location_dest_id': self.subcontract_move_id.location_dest_id.id,
-                'product_uom_qty': 0,
-                'total_weight': self.produced_weight,
-                'product_uom_id': self.product_uom_id.id,
-                'qty_done': self.qty_producing,
-                'lot_id': self.finished_lot_id and self.finished_lot_id.id,
-            })
-            if not self._get_todo(self.production_id):
-                ml_reserved = self.subcontract_move_id.move_line_ids.filtered(lambda ml:
-                    float_is_zero(ml.qty_done, precision_rounding=ml.product_uom_id.rounding) and
-                    not float_is_zero(ml.product_uom_qty, precision_rounding=ml.product_uom_id.rounding))
-                ml_reserved.unlink()
-                for ml in self.subcontract_move_id.move_line_ids:
-                    ml.product_uom_qty = ml.qty_done
-                self.subcontract_move_id._recompute_state()
-        return res
+            for ml in self.subcontract_move_id.move_line_ids:
+                ml.total_weight = self.produced_weight
+        if self.production_id:
+            for ml in self.production_id.move_finished_ids.move_line_ids:
+                ml.total_weight = self.produced_weight
+        return action
+
+    #def continue_production(self):
+     #   action = super(MrpProductProduce, self).continue_production()
+     #   action['context'] = dict(action['context'], default_subcontract_move_id=self.subcontract_move_id.id)
+     #   weight = 0
+     #   for line in self:
+     #       for move in line.subcontract_move_id.move_line_ids:
+     #           weight += move.total_weight
+     #   return action
+    
                         
 class MRPProductProduceLine(models.TransientModel):
     _inherit = 'mrp.product.produce.line'
