5d3bcfd636fcc2bb8cbde33c74a739587193eb0b
farhanrashad <farhan@dynexcel.com>
2020-04-23 00:30:11 +0000

    update quanitty total

--- de_document_quantity_total/__manifest__.py
+++ de_document_quantity_total/__manifest__.py
@@ -15,12 +15,13 @@
 
     'author': "Dynexcel",
     'website': "http://www.dynexcel.com",
+    "live_test_url": "https://www.youtube.com/watch?v=qjk0Lbg_5pM",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Inventory',
-    'version': '0.1',
+    'version': '0.2',
 
     # any module necessary for this one to work correctly
     'depends': ['base','sale','purchase','stock'],
