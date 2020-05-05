354f8001ec9de6b3b52cd1c1259a178dcc4a78f9
ramzannoon <ramzannoon675@gmail.com>
2020-05-05 16:41:05 +0000

    sale stock updated

--- de_sale_stock_report/models/.ipynb_checkpoints/sale_stock-checkpoint.py
+++ de_sale_stock_report/models/.ipynb_checkpoints/sale_stock-checkpoint.py
@@ -28,8 +28,7 @@ class PartnerLedger(models.TransientModel):
         required=True
     )
     location_id = fields.Many2one('stock.location', string='Select Location',)
-    sale_id = fields.Many2one('sale.order', string='Sale Order Reference',)
 
     def print_report(self, data=None):
-        data = {'start_date': self.start_date, 'end_date': self.end_date,'location_id': self.location_id.id,'sale_id': self.sale_id.id}
+        data = {'start_date': self.start_date, 'end_date': self.end_date,'location_id': self.location_id.id}
         return self.env.ref('de_sale_stock_report.sale_stock_pdf').report_action(self, data=data)
