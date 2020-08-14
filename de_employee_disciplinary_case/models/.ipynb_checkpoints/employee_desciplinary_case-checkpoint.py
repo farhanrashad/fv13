250aeb8bd335f00dd61a64867f123d4b907b4ee8
dynexcel-ddc1 <ddc1@dynexcel.co>
2020-08-14 12:12:15 +0000

    added

--- de_employee_disciplinary_case/models/.ipynb_checkpoints/employee_desciplinary_case-checkpoint.py
+++ de_employee_disciplinary_case/models/.ipynb_checkpoints/employee_desciplinary_case-checkpoint.py
@@ -15,4 +15,22 @@ class EmployeeDesciplinaryCase(models.Model):
     _name = 'hr.employee.disciplinary.case'
     _description = 'HR Employee Desciplinary Case'
 
-    name = fields.Char(string='Name', store=True, required=True)    
+    name = fields.Char(string='Name', store=True, required=True)
+    employee_id = fields.Many2one('hr.employee', string='Employee', store=True)
+    date = fields.Date(string='Date', store=True)
+    case_type = fields.Many2one('hr.employee.disciplinary.case.type',string='Case Type', store=True)
+    user_id = fields.Many2one('res.user', string='Issuer', store=True, required=True)
+    note = fields.Html(string="Description" )
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('waiting', 'Waiting'),
+        ('response', 'Response'),
+        ('close', 'Close'),
+    ], string='Status', readonly=True, copy=False, index=True, tracking=3, default='draft')
+    attachment_id = fields.Many2one('ir.attachment', string="Attachment", required=True)
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    test = fields.Char(string='Name', store=True, required=True)
+    
