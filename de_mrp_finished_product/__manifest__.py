226c47625cb553043a6da8a54791f49c56345eff
dynexcel-ddc1 <ddc1@dynexcel.co>
2020-08-20 12:11:52 +0500

    updated

--- de_mrp_finished_product/__manifest__.py
+++ de_mrp_finished_product/__manifest__.py
@@ -22,7 +22,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','mrp','stock'],
+    'depends': ['base','mrp','stock','account'],
 
     # always loaded
     'data': [
--- de_mrp_finished_product/models/account_move.py
+++ de_mrp_finished_product/models/account_move.py
@@ -6,8 +6,8 @@ from odoo import models, fields, api, _
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
-    roll = fields.Char(string='Roll')
-    pcs = fields.Char(string='PCS')
+    roll = fields.Char(string='Roll', store=True)
+    pcs = fields.Char(string='PCS', store=True)
 
 
 
