b3685aa8db213ea8e817b35e6c9430f9c116b46e
dynexcel-ddc1 <ddc1@dynexcel.co>
2020-08-24 10:44:32 +0000

    rfq updated

--- de_purchase_multiple_rfq/models/.ipynb_checkpoints/models-checkpoint.py
+++ de_purchase_multiple_rfq/models/.ipynb_checkpoints/models-checkpoint.py
@@ -5,71 +5,23 @@ from collections import defaultdict
 from odoo.exceptions import UserError
 
 
-# class PurchaseOrderExt(models.Model):
-#     _inherit = 'purchase.order'
-
-#     jo_sheet_reference = fields.Char(string='Reference')
 
 
-class JobOrderSheet(models.Model):
+
+class PurchaseOrderMultiple(models.Model):
     _name = 'purchase.order.multiple'
     _description = 'Multiple Purchase Order'
 
     @api.model
     def create(self, vals):
         if vals.get('name', 'New') == 'New':
-            vals['name'] = self.env['ir.sequence'].next_by_code('job.order.sheet') or 'New'
-        result = super(JobOrderSheet, self).create(vals)
+            vals['name'] = self.env['ir.sequence'].next_by_code('purchase.order.multiple') or 'New'
+        result = super(PurchaseOrderMultiple, self).create(vals)
         return result
 
-    def get_sheet_lines(self):
-        for rec in self:
-            rec.sheet_ids.unlink()
-            order_data = self.env['mrp.production'].search([('sale_id', '=', rec.sale_order_id.name),
-                                                            '|',
-                                                            '|',
-                                                            ('product_id.name', '=ilike', ' %'),
-                                                            ('product_id.name', '=ilike', 'Module%'),
-                                                            '|',
-                                                            ('product_id.name', '=ilike', '[Un-Finished]%'),
-                                                            ('product_id.name', '=ilike', '[Module]%')])
-            for order in order_data:
-                rec.sheet_ids |= rec.sheet_ids.new({
-                    'mo_order_id': order.id,
-                    'product_id': order.product_id.id,
-                    'product_quantity': order.product_qty,
-                })
-            rec.write({
-                'progress': 'done',
-            })
+    
 
-    def action_approve(self):
-        self.state = 'approved'
-
-    def action_completed(self):
-        self.state = 'done'
-
-    def action_quantity_update(self):
-        for line in self.sheet_ids:
-            print(line.product_name)
-            update_qty = line.in_house_production + line.outsource_production
-            order = self.env['mrp.production'].search([('id', '=', line.mo_order_id.id)])
-            order.update({
-                'product_qty': line.in_house_production
-            })
-            stock_picking = self.env['stock.picking'].search([('origin', '=', line.mo_order_id.name),
-                                                              ('picking_type_id', '=', 10)])
-            print('stock', stock_picking)
-            for picking in stock_picking:
-                for pick_line in picking.move_ids_without_package:
-                    print('qty', line.in_house_production)
-                    pick_line.update({
-                        'product_uom_qty': line.in_house_production * 2
-                    })
-            for qty in order.move_raw_ids:
-                qty.update({
-                    'product_uom_qty': line.in_house_production
-                })
+    
 
     def action_generate_po(self):
         for line in self.sheet_ids:
@@ -101,16 +53,6 @@ class JobOrderSheet(models.Model):
     name = fields.Char(
         'Reference', copy=False, readonly=True, default=lambda x: _('New'))
     date = fields.Date(string='Date', required=True)
-    sale_order_id = fields.Many2one(comodel_name='sale.order', string='Sale Order')
-    state = fields.Selection([
-        ('draft', 'Draft'),
-        ('approved', 'Approved'),
-        ('done', 'Completed')],
-        readonly=True, string='State', default='draft')
-    progress = fields.Selection([
-        ('in_progress', 'In Progress'),
-        ('done', 'Done')], 'Progress',
-        default='in_progress')
     po_created = fields.Boolean(string='PO Created')
     space = fields.Char(default=" ", readonly=True)
     sheet_ids = fields.One2many(comodel_name='purchase.order.multiple.line', inverse_name='sheet_id')
@@ -120,21 +62,10 @@ class JobOrderSheetLine(models.Model):
     _name = 'purchase.order.multiple.line'
     _description = 'Material Planning'
 
-    def update_product_quantity(self):
-        for rec in self:
-            self.write({
-                'product_quantity': rec.update_quantity,
-            })
-            order = self.env['mrp.production'].search([('id', '=', rec.mo_order_id.id)])
-            order.update({
-                'product_qty': rec.update_quantity,
-            })
+
 
     sheet_id = fields.Many2one(comodel_name='purchase.order.multiple')
-#     mo_order_id = fields.Many2one(comodel_name='mrp.production', string='Reference', required=True)
     product_id = fields.Many2one(comodel_name='product.product', string='Product')
     product_name = fields.Char(string='Product Name', related='product_id.name')
     product_quantity = fields.Float(string='Quantity')
-#     in_house_production = fields.Float(string='InHouse Production')
-#     outsource_production = fields.Float(string='Outsource Production')
-    vendor_id = fields.Many2one(comodel_name='res.partner', string='Vendor')
+    vendor_id = fields.Many2one(comodel_name='res.partner', string='Vendor', required=True)
