d8fcd8da2cd02d31e36bafd35068a99d001c19dc
dynexcel-ddc1 <ddc1@dynexcel.co>
2020-09-21 03:31:38 +0000

    update po

--- de_mrp_po_generation/models/models.py
+++ de_mrp_po_generation/models/models.py
@@ -7,12 +7,21 @@ class MoBeforhandWizard(models.TransientModel):
     _name = 'mrp.mo.beforehand.wizard'
     _description = 'Create PO from MO'
 
-    mrp_id = fields.Many2one('mrp.production',string="Manufacturing Order")
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    mrp_id = fields.Many2many('mrp.production',string="Manufacturing Order")
+    mo_line_ids = fields.One2many('mrp.mo.beforehand.wizard.line','mo_id',string="Manufacturing Order")
+
+    
+
+    
+    
+    
+class MoBeforhandWizardLine(models.TransientModel):
+    _name = 'mrp.mo.beforehand.wizard.line'
+    _description = 'Create PO from MO'
+
+    product_id = fields.Many2one('product.product',string="Product")
+    product_uom_qty = fields.Float(string='Qty to consume')
+    on_hand_qty = fields.Float(string="Quantity On Hand")
+    forcast_qty = fields.Float(string="Forcast Quantity")
+    mo_id = fields.Many2one('mrp.mo.beforehand.wizard',string="Product")    
+
