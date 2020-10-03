639f3cd36f102a08d7b3b78778c9cd1ff6a0c66c
dynexcel-ddc1 <ddc1@dynexcel.co>
2020-10-03 09:27:20 +0500

    update   discount

--- de_discount_invoices/__manifest__.py
+++ de_discount_invoices/__manifest__.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "de_discount_invoices",
+    'name': "Discount Invoice",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Discount on invoice""",
 
     'description': """
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Dynexcel",
+    'website': "http://www.dynexcel.co",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -25,8 +24,6 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
         'views/discount_invoices_view.xml',
 
     ],
