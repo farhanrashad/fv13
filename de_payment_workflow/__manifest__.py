6de709d9bc1c6c15907498209af23e2bca36b2e9
dynexcel-ddc1 <ddc1@dynexcel.co>
2020-10-05 08:24:24 +0500

    update workflow

---
+++ de_payment_workflow/__manifest__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+# Author      : Dynexcel (<https://dynexcel.com/>)
+# Copyright(c): 2015-Present dynexcel.com
+# All Rights Reserved.
+#
+#
+#
+# This program is copyright property of the author mentioned above.
+# You can`t redistribute it and/or modify it.
+#
+#
+# You should have received a copy of the License along with this program.
+#################################################################################
+{
+  "name"                 :  "Payment Approval Workflow",
+  "summary"              :  "This Module add the approval workflow functionality in payment Transaction",
+  "category"             :  "Accounting",
+  "version"              :  "1.4",
+  "sequence"             :  1,
+  "author"               :  "Dynexcel",
+  "license"              :  "AGPL-3",
+  "website"              :  "http://dynexcel.com",
+  "description"          :  """
+
+""",
+  "live_test_url"        :  "",
+  "depends"              :  [
+                             'account',
+                            ],
+  "data"                 :  [
+                            'views/payment_state_view.xml',
+                            'views/payment_view.xml',
+                            'views/payment_group_view.xml'
+                            ],
+  "images"               :  ['static/description/Banner.png'],
+  "application"          :  True,
+  "installable"          :  True,
+  "auto_install"         :  False,
+  "price"                :  0,
+  "currency"             :  "EUR",
+  "images"		 :['static/description/banner.jpg'],
+}
