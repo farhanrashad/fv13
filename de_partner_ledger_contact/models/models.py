e3adeab9d6c74b014002913d052f25d340efc795
dynexcel-ddc1 <64604276+dynexcel-ddc1@users.noreply.github.com>
2020-11-12 23:58:39 +0500

    update de_partner_ledger_contact

--- de_partner_ledger_contact/models/models.py
+++ de_partner_ledger_contact/models/models.py
@@ -1,16 +1,93 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api, _
+from odoo import models, api, _, _lt, fields
+from odoo.tools.misc import format_date
+from datetime import timedelta
 
 
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
     
-    city = fields.Char(string="City", related='partner_id.city')
+    contact = fields.Char(string="Contact", related='partner_id.phone')
 
 
 class AccountReportLedger(models.AbstractModel):
     _inherit = 'account.partner.ledger'
+    
+    
+    ####################################################
+    # COLUMNS/LINES
+    ####################################################
+
+    @api.model
+    def _get_report_line_partner(self, options, partner, initial_balance, debit, credit, balance):
+        company_currency = self.env.company.currency_id
+        unfold_all = self._context.get('print_mode') and not options.get('unfolded_lines')
+
+        columns = [
+#             {'name': partner.phone},
+            {'name': self.format_value(initial_balance), 'class': 'number'},
+            {'name': self.format_value(debit), 'class': 'number'},
+            {'name': self.format_value(credit), 'class': 'number'},
+        ]
+        if self.user_has_groups('base.group_multi_currency'):
+            columns.append({'name': ''})
+        columns.append({'name': self.format_value(balance), 'class': 'number'})
+
+        res = {
+            'id': 'partner_%s' % (partner.id if partner else 0),
+            'name': partner is not None and ((partner.name + ' (' + partner.phone + ')' or ''))[:128] or _('Unknown Partner'),
+            'columns': columns,
+            'level': 2,
+            'trust': partner.trust if partner else None,
+            'unfoldable': not company_currency.is_zero(debit) or not company_currency.is_zero(credit),
+            'unfolded': 'partner_%s' % (partner.id if partner else 0) in options['unfolded_lines'] or unfold_all,
+            'colspan': 6,
+        }
+        return res
+
+    
+    @api.model
+    def _get_report_line_move_line(self, options, partner, aml, cumulated_init_balance, cumulated_balance):
+        if aml['payment_id']:
+            caret_type = 'account.payment'
+        elif aml['move_type'] in ('in_refund', 'in_invoice', 'in_receipt'):
+            caret_type = 'account.invoice.in'
+        elif aml['move_type'] in ('out_refund', 'out_invoice', 'out_receipt'):
+            caret_type = 'account.invoice.out'
+        else:
+            caret_type = 'account.move'
+
+        date_maturity = aml['date_maturity'] and format_date(self.env, fields.Date.from_string(aml['date_maturity']))
+        columns = [
+            {'name': aml['journal_code']},
+            {'name': aml['account_code']},
+            {'name': self._format_aml_name(aml['name'], aml['ref'], aml['move_name'])},
+#             {'name': aml['partner_contact']},
+            {'name': date_maturity or '', 'class': 'date'},
+            {'name': aml['full_rec_name'] or ''},
+            {'name': self.format_value(cumulated_init_balance), 'class': 'number'},
+            {'name': self.format_value(aml['debit'], blank_if_zero=True), 'class': 'number'},
+            {'name': self.format_value(aml['credit'], blank_if_zero=True), 'class': 'number'},
+        ]
+        if self.user_has_groups('base.group_multi_currency'):
+            if aml['currency_id']:
+                currency = self.env['res.currency'].browse(aml['currency_id'])
+                formatted_amount = self.format_value(aml['amount_currency'], currency=currency, blank_if_zero=True)
+                columns.append({'name': formatted_amount, 'class': 'number'})
+            else:
+                columns.append({'name': ''})
+        columns.append({'name': self.format_value(cumulated_balance), 'class': 'number'})
+        return {
+            'id': aml['id'],
+            'parent_id': 'partner_%s' % (partner.id if partner else 0),
+            'name': format_date(self.env, aml['date']),
+            'class': 'date' + aml.get('class', ''),
+            'columns': columns,
+            'caret_options': caret_type,
+            'level': 4,
+        }
     
     @api.model
     def _get_query_amls(self, options, expanded_partner=None, offset=None, limit=None):
@@ -26,7 +103,7 @@ class AccountReportLedger(models.AbstractModel):
 
         # Get sums for the account move lines.
         # period: [('date' <= options['date_to']), ('date', '>=', options['date_from'])]
-        if expanded_partner:
+        if expanded_partner is not None:
             domain = [('partner_id', '=', expanded_partner.id)]
         elif unfold_all:
             domain = []
@@ -45,7 +122,7 @@ class AccountReportLedger(models.AbstractModel):
                 account_move_line.name,
                 account_move_line.ref,
                 account_move_line.company_id,
-                account_move_line.account_id,             
+                account_move_line.account_id,
                 account_move_line.payment_id,
                 account_move_line.partner_id,
                 account_move_line.currency_id,
@@ -56,6 +133,7 @@ class AccountReportLedger(models.AbstractModel):
                 account_move_line__move_id.name         AS move_name,
                 company.currency_id                     AS company_currency_id,
                 partner.name                            AS partner_name,
+                partner.phone                           AS partner_contact,
                 account_move_line__move_id.type         AS move_type,
                 account.code                            AS account_code,
                 account.name                            AS account_name,
@@ -71,7 +149,7 @@ class AccountReportLedger(models.AbstractModel):
             LEFT JOIN account_journal journal           ON journal.id = account_move_line.journal_id
             LEFT JOIN account_full_reconcile full_rec   ON full_rec.id = account_move_line.full_reconcile_id
             WHERE %s
-            ORDER BY account_move_line.date ASC, account_move_line.id 
+            ORDER BY account_move_line.date, account_move_line.id
         ''' % (ct_query, where_clause)
 
         if offset:
@@ -83,5 +161,22 @@ class AccountReportLedger(models.AbstractModel):
 
         return query, where_params
 
-   
+    def _get_columns_name(self, options):
+        columns = [
+            {},
+            {'name': _('JRNL')},
+            {'name': _('Account')},
+            {'name': _('Ref')},
+#             {'name': _('Contact')},
+            {'name': _('Due Date'), 'class': 'date'},
+            {'name': _('Matching Number')},
+            {'name': _('Initial Balance'), 'class': 'number'},
+            {'name': _('Debit'), 'class': 'number'},
+            {'name': _('Credit'), 'class': 'number'}]
 
+        if self.user_has_groups('base.group_multi_currency'):
+            columns.append({'name': _('Amount Currency'), 'class': 'number'})
+
+        columns.append({'name': _('Balance'), 'class': 'number'})
+
+        return columns
