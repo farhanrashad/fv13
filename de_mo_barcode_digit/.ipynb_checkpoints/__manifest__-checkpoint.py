58e0da3562ec6762a4cab98fcf7f3364e43b3333
dynexcel-ddc1 <ddc1@dynexcel.co>
2020-11-15 13:22:56 +0000

    updated

---
+++ de_mo_barcode_digit/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "MO Barcode Digit",
+    "category": 'Education',
+    "summary": 'MO Barcode Digit Summary',
+    "description": """
+	 
+   
+    """,
+    "sequence": 1,
+    "author": "Dynexcel",
+    "website": "http://www.dynexcel.co",
+    "version": '13.1.0.0',
+    "depends": ['base'],
+    "data": [
+        # 'security/ir.model.access.csv',
+        'views/barcode_digit_view.xml',
+    ],
+
+    "price": 25,
+    "currency": 'EUR',
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
