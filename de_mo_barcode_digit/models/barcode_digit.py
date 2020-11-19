f6e665ee9c910ec70d7eabdbcdbdd23b5dcb7a0a
dynexcel-ddc1 <ddc1@dynexce.co>
2020-11-19 12:14:02 +0500

    update security

--- de_mo_barcode_digit/models/barcode_digit.py
+++ de_mo_barcode_digit/models/barcode_digit.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# from custom_addons import de_training_batch
+from custom_addons import de_training_batch
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError
 
