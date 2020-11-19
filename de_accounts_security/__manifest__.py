2eb1a894cb04339401dadc7bd864527c2efaa58a
dynexcel-ddc1 <ddc1@dynexcel.co>
2020-11-19 07:38:01 +0000

    update accounts

---
+++ de_accounts_security/__manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "User Restriction",
+
+    'summary': """
+        Allow user to register payment in invoice
+        """,
+
+    'description': """
+        Allow user to register payment in invoice
+        1-spainabottle2
+        
+    """,
+
+    'author': "Dynexcel",
+    'website': "http://www.dynexcel.co",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Extra Tools',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','account','account_accountant','purchase','sale'],
+
+    # always loaded
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+#         'views/views.xml',
+#         'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
