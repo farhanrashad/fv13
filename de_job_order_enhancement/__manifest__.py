76e1bb612a5438444110935723cc7ae355b3f2f4
dynexcel-ddc1 <ddc1@dynexcel.co>
2020-11-20 03:34:23 +0000

    update enhacement

--- de_job_order_enhancement/__manifest__.py
+++ de_job_order_enhancement/__manifest__.py
@@ -1,22 +1,27 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "de_job_order_enhancement",
+    'name': "Job Order Enhancement",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Job Order Enhancement 
+        Get All Component of BOM in Material Planning
+        """,
 
     'description': """
-        Long description of module's purpose
+        Job Order Enhancement 
+        1- Get component of BOM in hierarchical manner
+        2- All component of BOM store in Material Planning Tab
+        3- Component Having type Subcontracting  Purchase Order created and 
+        Component having type Manufacturing  Production Order Created
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Dynexcel",
+    'website': "http://www.dynexcel.co",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Manufacturing',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -24,7 +29,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
