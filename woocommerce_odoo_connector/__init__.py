94a7dd2d34fbef30546f514f3f261e19ea3c17ef
Farhan Rashad <farhan@dynexcel.com>
2021-01-01 14:19:14 +0500

    remove modules

--- woocommerce_odoo_connector/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#################################################################################
-#
-#    Copyright (c) 2016-Present Webkul Software Pvt. Ltd. (<https://webkul.com/>)
-#
-#################################################################################
-
-from . import models,wizard
