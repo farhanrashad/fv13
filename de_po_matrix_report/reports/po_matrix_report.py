df32f03b5eb25896eb9e2e5c166ce4386dd4fd73
dynexcel-ddc1 <ddc1@dynexcel.co>
2021-01-27 10:26:58 +0000

    Updated

--- de_po_matrix_report/reports/po_matrix_report.py
+++ de_po_matrix_report/reports/po_matrix_report.py
@@ -15,7 +15,7 @@ class PurchaseOrderInh(models.Model):
 class POMatrixReport(models.AbstractModel):
     _name = 'report.de_po_matrix_report.po_matrix_report_templet'
     
-    def get_matched_record(self, color, model):
+    def get_qty(self, color, model):
         self.model = self.env.context.get('active_model')
         docs = self.env[self.model].browse(self.env.context.get('active_id'))
         records_list = []
@@ -48,6 +48,29 @@ class POMatrixReport(models.AbstractModel):
             return rec.total_weight
         else:
             return 0.0
+    
+    def get_weight_unit(self, model):
+        self.model = self.env.context.get('active_model')
+        docs = self.env[self.model].browse(self.env.context.get('active_id'))
+        records_list = []
+#         raise UserError((model))
+        for record in docs.order_line:
+            if record.display_type != "line_section":
+                data = record.product_id.name
+                x = re.search(model, data)
+#                 y = re.search(color, data)
+                if x:
+                    records_list.append(record.id)
+#         raise UserError((records_list[0]))
+        rec = self.env['purchase.order.line'].browse(records_list[0])
+#         raise UserError(("Rec " +rec))
+#         raise UserError((rec))
+        if rec:
+            return rec.weight
+        else:
+            return 0.0
+        
+        
         
     
     def _get_report_values(self, docids, data=None):
@@ -70,14 +93,15 @@ class POMatrixReport(models.AbstractModel):
         
         color_list = list(dict.fromkeys(color_list))
         model_list = list(dict.fromkeys(model_list))
-
+#         raise UserError((model_list))
         return {
                 'docs': docs,
 #                 'orders': invoices,
                 'model_list':model_list,
                 'color_list': color_list,
-                'product_qty': self.get_matched_record,
-                'product_weight': self.get_weight
+                'product_qty': self.get_qty,
+                'product_weight': self.get_weight,
+                'product_unit': self.get_weight_unit
             }
         
             
