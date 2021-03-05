eefbe19cf086f7282e3e311863d7bc7e39c2c505
dynexcel-ramzan <m.ramzan@dynexcel.co>
2021-03-05 12:49:33 +0500

    update check printing

--- account_check_printing/models/account_payment.py
+++ account_check_printing/models/account_payment.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+
+
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError, ValidationError
 from odoo.tools.misc import formatLang, format_date
