566b6e3ba70b8c1989e709d3501a16d55c31cabe
dynexcel-ddc1 <ddc1@dynexcel.co>
2021-03-05 12:19:01 +0000

    update je cheque

--- de_je_cheque_print/models/.ipynb_checkpoints/cheque_print-checkpoint.py
+++ de_je_cheque_print/models/.ipynb_checkpoints/cheque_print-checkpoint.py
@@ -7,3 +7,6 @@ class JeChequePrint(models.Model):
     _inherit = 'account.move'
 
     is_cheque_print = fields.Boolean(string='Cheque Print', default=False)
+    payee = fields.Char(string='Payee')
+    amount = fields.Float(string='Amount')
+    cheque_format_id = fields.Many2one('cheque.setting', string='Cheque Format')
