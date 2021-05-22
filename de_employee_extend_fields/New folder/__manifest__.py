679464d3fcc5ff8bd7e5328928d5a0c8913ee270
Fawaad Suleman <fawaad.s@dynexcel.co>
2021-05-22 14:53:15 +0500

    de_employee_extend_fields

---
+++ de_employee_extend_fields/New folder/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Employee Extend Fields",
+
+    'summary': """
+        Employee Extend Feilds and emergency contact details""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "Dynexcel",
+    'website': "http://www.dynexcel.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/11.0/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Hr',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','hr'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
---
+++ de_employee_extend_fields/New folder/models.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class de_employee_extend_fields(models.Model):
+    _inherit = 'hr.employee'
+
+    father_name = fields.Char(string="Father Name")
+    permanent_address = fields.Char(string="Permanent Address")
+    qualification = fields.Char(string="Qualifiation")
+    number_of_department = fields.Char(string="Number Of Department")
+    emergency_contact_lines = fields.One2many('hr.employee.emergency.contact', 'hr_employee_id', string='Emergency Contact Lines', copy=True, auto_join=True)
+    
+    
+class HrEmergencyCOntact(models.Model):
+    _name='hr.employee.emergency.contact'
+    
+    hr_employee_id = fields.Many2one('hr.employee', string='Employee Reference', required=False, ondelete='cascade', index=True, copy=False, readonly=True)
+    name = fields.Char(string="Name")
+    relation = fields.Char(string="Relation")
+    mobile_number = fields.Char(string="Mobile Number")
+    address = fields.Char(string="Address")
