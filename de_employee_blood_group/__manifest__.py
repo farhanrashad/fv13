973b9fe34d3f820a08c898b284bd8b1a6767dde8
Fawaad Suleman <fawaad.s@dynexcel.co>
2021-05-22 15:22:14 +0500

    de_employee_blood_group

---
+++ de_employee_blood_group/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name' : 'Employee Blood Group',
+    'version' : '1.0',
+    'author' : 'DX',
+    'website' : '',
+    'category' : '',
+    'description' : """
+	Employee Blood Group
+    """,
+    'depends' : ['base','hr'],
+    'demo' : [],
+    'data' : [
+            'views/employee_view.xml'
+        ],
+    'installable' : True,
+}
+
