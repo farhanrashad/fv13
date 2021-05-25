7341e8e2f1664f91f64babc49fd8a2ced68116d7
Fawaad Suleman <fawaad.s@dynexcel.co>
2021-05-25 11:46:52 +0500

    de_employee_blood_group

--- de_employee_blood_group/__manifest__.py
+++ de_employee_blood_group/__manifest__.py
@@ -1,17 +1,30 @@
+# -*- coding: utf-8 -*-
 {
-    'name' : 'Employee Blood Group',
-    'version' : '1.0',
-    'author' : 'DX',
-    'website' : '',
-    'category' : '',
-    'description' : """
-	Employee Blood Group
-    """,
-    'depends' : ['base','hr'],
-    'demo' : [],
-    'data' : [
-            'views/employee_view.xml'
-        ],
-    'installable' : True,
-}
+    'name': "de_employee_blood_group",
 
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "Dynexcel",
+    'website': "https://www.dynexcel.co",
+    'category': 'hr',
+    'version': '13.0.0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['hr'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/employee_view.xml'
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
